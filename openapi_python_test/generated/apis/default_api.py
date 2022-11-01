82a545d65bb642e725d629b8ddd6983aa10c3bfa
sherifnada <sherif@MacBook-Pro-2.local>
2022-10-31 23:24:31 -0700

    clean things up

--- openapi_python_test/generated/apis/default_api.py
+++ openapi_python_test/generated/apis/default_api.py
@@ -1,7 +1,7 @@
 # coding: utf-8
 
 from abc import ABC, abstractmethod
-from typing import Dict, List  # noqa: F401
+from typing import Callable, Dict, List  # noqa: F401
 
 from fastapi import (  # noqa: F401
     APIRouter
@@ -11,20 +11,39 @@ from openapi_python_test.generated.models.hello_world_get200_response import Hel
 import inspect
 
 class DefaultApi(ABC): 
-  # autogenned, contains pythonic signature of api
   @abstractmethod
   async def hello_world(self, *args, **kwargs) -> HelloWorldGet200Response:
      """ hello world """
 
 
+def _set_signature_if_not_set(method: Callable) -> None:
+    """
+    APIRouter().add_api_route expects the input method to have a signature. It gets signatures
+    by running inspect.signature(method) under the hood. 
+
+    In the case that an instance method does not declare "self" as an input parameter (due to developer error 
+    for example), then the call to inspect.signature() fails. 
+
+    This method is sort of a workaround. It tells the developer how to fix the problem if it's detected.
+    """
+    try:
+        print('0')
+        inspect.signature(method)
+    except ValueError as e:
+        # Based on empirical observation, the call to inspect fails with a ValueError
+        # with exactly one argument: "invalid method signature"
+        if e.args and len(e.args) == 1 and e.args[0] == "invalid method signature":
+            # I couldn't figure out how to setattr on a "method" object to populate the signature. For now just kick
+            # it back to the developer and tell them to set the "self" variable
+            raise Exception(f"Method {method.__name__} in class {type(method.__self__).__name__} must declare the variable 'self'. ")
+        else:
+            raise
+
+
 def initialize_router(api: DefaultApi) -> APIRouter:
     router = APIRouter()
-    # Directly add the method as a route implementation 
-    # instead of adding via decoration (which is the current approach)
-    print("HII")
-    print(api.hello_world)
-    # setattr(api, "__signature__")
-
+    
+    _set_signature_if_not_set(api.hello_world)
     router.add_api_route(
         "/hello_world",
         endpoint=api.hello_world,
@@ -33,12 +52,5 @@ def initialize_router(api: DefaultApi) -> APIRouter:
             200: {"model": HelloWorldGet200Response, "description": "Successful operation"},
         }
     )
-    
-    
-    # for each endpoint, do the same thing...
-    # ...
-    
-    # at the end just return the router
-    print("HELLOOOOO")
     
     return router
